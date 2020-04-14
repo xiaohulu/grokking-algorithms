d5dbcf17a7073ba8e9c6c1d38ffb4b9177bd44e0
xiaohulu <idocument@qq.com>
2020-04-14 20:48:47 +0800

    简化代码

--- src/ch04/quick_sort.rs
+++ src/ch04/quick_sort.rs
@@ -3,9 +3,8 @@ pub fn quick_sort(arr: Vec<i32>) -> Vec<i32> {
         return arr;
     }
     let pivot = arr[0];
-    let (less, greater): (Vec<i32>, Vec<i32>) = arr[1..].iter().partition(|&&el| el<=pivot);
-    let result = [quick_sort(less), quick_sort(greater)].join(&pivot);
-    return result;
+    let (less, greater): (Vec<i32>, Vec<i32>) = arr[1..].iter().partition(|&&el| el <= pivot);
+    [quick_sort(less), quick_sort(greater)].join(&pivot)
 }
 
 #[cfg(test)]
@@ -17,5 +16,4 @@ mod tests {
     fn quick_sort_success() {
         assert_eq!(quick_sort(vec![10, 5, 2, 3]), [2, 3, 5, 10]);
     }
-
 }
